3793763068cb1b60c7668c9f119784f53cb20a33
λ Justin Ventura <55901784+jventura1738@users.noreply.github.com>
2021-07-13 19:48:09 -0400

    Update jenna.py

--- jenna.py
+++ jenna.py
@@ -3,4 +3,5 @@
 if __name__ == '__main__':
   print('jenna likes turtles')
   print('who is joe?\n'*123)
-  print("Jenna is awsome")
+  print("Jenna is awesome")
+  print("Jenna thought 'oui oui' was 'wee wee' until this year LOL")
