65a1de3e80352c5b936ebfadd8e53c259f462087
jennabustami <jenna.bustami@gmail.com>
2021-07-13 17:13:12 -0700

    removing cringe

--- jenna.py
+++ jenna.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
   print('jenna likes turtles')
   print('who is joe?\n'*123)
   print("Jenna is awesome")
-  print("Jenna thought 'oui oui' was 'wee wee' until this year LOL")
+  print("Jenna thought 'oui oui' was 'wee wee' until this year")
