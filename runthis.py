201c31aa896eb0c174945ca920d6b735bbf8b00b
Jenna <jenna.bustami@gmail.com>
2021-07-26 22:59:17 -0700

    fixed broken code

--- runthis.py
+++ runthis.py
@@ -1,13 +1,12 @@
 from random import choice
-#run to see facts
-jenna= ["awesome", "cool", "amazing", "the best person ever", "hot asf", "smart asf", "going to the best university ever", "the nicest person ever"]
-justin= ["weird", "odd", "cringy", "irrelevant", "low iq", "annoying", "living on the wost coast", "going to apply to berkeley"]
+# run to see facts
+jenna = ["annoying", "okay", "jenna", "mid", "smart but not as smart as justin", "going to an okay school", "the meanest person ever"]
+justin = ["interesting", "fun", "funny", "important", "high iq", "living on the best coast", "going to apply to berkeley (in jennas dreams)"]
 
-name = input("jenna or justin?")
-if name== "jenna":
-    print( name, "is", choice(jenna))
+name = input("jenna or justin? ")
+if name == "jenna":
+    print(name, "is", choice(jenna))
 elif name == "justin":
     print(name, "is", choice(justin))
 else:
     print("joe mama")
-
