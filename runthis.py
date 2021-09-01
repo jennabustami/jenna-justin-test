ae18e248be3822da9c30bf65a858ccd5bd2ccc9d
j.ventura1738 <justinv9001@aol.com>
2021-09-01 09:20:27 -0400

    fixed jenna's broken code for her smh

--- runthis.py
+++ runthis.py
@@ -1,11 +1,16 @@
 from random import choice
-#run to see facts
-jenna= ["awesome", "cool", "amazing", "the best person ever", "hot asf", "smart asf", "going to the best university ever", "the nicest person ever"]
-justin= ["weird", "odd", "cringy", "irrelevant", "low iq", "annoying", "living on the wost coast", "going to apply to berkeley"]
+
+# The code below is broken:
+# jenna = ["awesome", "cool", "amazing", "the best person ever", "hot asf", "smart asf", "going to the best university ever", "the nicest person ever"]
+# justin = ["weird", "odd", "cringy", "irrelevant", "low iq", "annoying", "living on the wost coast", "going to apply to berkeley"]
+
+# Here is the fixed version:
+jenna = ['annoying', 'cringe', 'weird and sleeps on the floor', 'is basically a turtle', 'is broke', 'pays $20K a year on dorms', 'needs to drink more water']
+jusin = ['amazing', 'big IQ', 'sleeps in a bed unlike jenna', 'is built different', 'will fold jenna in half', 'is GOATED', 'knows who joe is']
 
 name = input("jenna or justin?")
-if name== "jenna":
-    print( name, "is", choice(jenna))
+if name == "jenna":
+    print(name, "is", choice(jenna))
 elif name == "justin":
     print(name, "is", choice(justin))
 else:
